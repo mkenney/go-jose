6c5205edb1d0b2979544dc7bd2a5f119756c8bf9
Michael Kenney <michael.kenney@returnpath.com>
2017-11-19 17:53:52 -0700

    docs

---
+++ .doc.go
@@ -0,0 +1,29 @@
+/*
+Package go-jose contains tools for implementing the various JOSE technologies
+such as JWT, JWS, JWE, etc in Go.
+
+JWS (https://tools.ietf.org/html/rfc7515)
+JWE (https://tools.ietf.org/html/rfc7516)
+JWK (https://tools.ietf.org/html/rfc7517)
+JWA (https://tools.ietf.org/html/rfc7518)
+JWT (https://tools.ietf.org/html/rfc7519)
+Examples (https://tools.ietf.org/html/rfc7520)
+
+The primary goal of this library is to provide a complete, easy to use,
+Javascript Object Signing and Encryption (JOSE) implementation. Existing
+libraries are either incomplete, poorly documented, or difficult to implement.
+
+	https://github.com/dgrijalva/jwt-go
+	https://github.com/lestrrat/go-jwx
+	https://github.com/SermoDigital/jose
+	https://github.com/square/go-jose
+
+This library exists because there was a need for a toolset that encompasses the
+whole set of JWx technologies in a highly customizable manner, in one package.
+*/
+package go-jose
+
+/*
+Version describes the version of this library.
+*/
+const Version = "0.0.1"
