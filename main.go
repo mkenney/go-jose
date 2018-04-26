202a45aeac1f69a5f9891b5a9990db3c2a4053ee
Michael Kenney <michael.kenney@returnpath.com>
2018-04-26 15:44:48 -0600

    saving

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
-import (
-	log "github.com/Sirupsen/logrus"
-
-	"github.com/mkenney/go-jose/crypto"
-	"github.com/mkenney/go-jose/jws"
-)
-
-func main() {
-	log.Debugf(crypto.HS256.Alg())
-
-	header := jws.NewProtectedHeader()
-	log.Debugf("Set result: %s", header.SetAlg("asdf"))
-
-	tmp, err := header.MarshalJSON()
-	log.Debugf("Claims: %s, %s", tmp, err)
-}
+//import (
+//	log "github.com/Sirupsen/logrus"
+//
+//	"github.com/mkenney/go-jose/crypto"
+//	"github.com/mkenney/go-jose/jws"
+//)
+//
+//func main() {
+//	log.Debugf(crypto.HS256.Alg())
+//
+//	header := jws.NewProtectedHeader()
+//	log.Debugf("Set result: %s", header.SetAlg("asdf"))
+//
+//	tmp, err := header.MarshalJSON()
+//	log.Debugf("Claims: %s, %s", tmp, err)
+//}
